5b560ee162f05490333341767d72dc53aede716a
alexsirr <alexsirruw@gmail.com>
2017-10-19 15:27:07 -0700

    class stuff

--- hmac/main_test.go
+++ hmac/main_test.go
@@ -1,12 +1,23 @@
 package main
 
 import "testing"
+import "strings"
 
 func TestSign(t *testing.T) {
 	//TODO: write unit test cases for sign()
 	//use strings.NewReader() to get an io.Reader
 	//interface over a simple string
 	//https://golang.org/pkg/strings/#NewReader
+	signingKey := "thisisthekey"
+	sig, _ := sign(signingKey, strings.NewReader("testing"))
+	sig2, _ := sign(signingKey, strings.NewReader("testing"))
+	if sig != sig2 {
+		t.Errorf("Same string did not produce the same sig")
+	}
+	sig2, _ = sign(signingKey, strings.NewReader("nottesting"))
+	if sig == sig2 {
+		t.Errorf("Change in test string led to same signature")
+	}
 }
 
 func TestVerify(t *testing.T) {
@@ -14,4 +25,10 @@ func TestVerify(t *testing.T) {
 	//use strings.NewReader() to get an io.Reader
 	//interface over a simple string
 	//https://golang.org/pkg/strings/#NewReader
+	signingKey := "thisisthekey"
+	sig, _ := sign(signingKey, strings.NewReader("testing"))
+	valid, _ := verify(signingKey, sig, strings.NewReader("testing"))
+	if !valid {
+		t.Error("Signature was invalid")
+	}
 }
